c8fb13df6eb38bc58d83f4fe0db24514480a780b
Erika Valle-Baird <airikuh86@gmail.com>
2023-04-13 11:07:07 -0400

    Added new test file to work in different testing branches

---
+++ newTests.java
@@ -0,0 +1,17 @@
+package SelectSort;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class testSelectionSort {
+	
+	@Test
+	public void test() {
+
+	}
+
+    public testSelectionSort() {
+    }
