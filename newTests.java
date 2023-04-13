123d00545d3966606e849d773ac82a04a562fed1
Erika Valle-Baird <airikuh86@gmail.com>
2023-04-13 11:18:48 -0400

    Testing for Mixed and Duplicates completed in Atom

--- newTests.java
+++ newTests.java
@@ -6,12 +6,68 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class testSelectionSort {
-	
+public class newTests {
+
 	@Test
 	public void test() {
+		testMixed();
+		testDuplicates();
 
 	}
 
-    public testSelectionSort() {
+    public newTests() {
     }
+		public void testMixed(){
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = -9;
+        Sortedarr[4] = 10;
+
+        /** Test data contains with both positive, negative and zeros **/
+        int[] tempArr = new int[5];
+        SelectionSort temp1 = new SelectionSort();
+        tempArr = temp1.basicSelectionSort(arr);
+        for(int i = 0; i < arr.length; i++) {
+        	assertTrue((arr[i] > 0) || (arr[i] < 0));
+        	assertTrue((Sortedarr[i] > 0) || (Sortedarr[i] < 0));
+        }
+
+    }
+
+    public void testDuplicates(){
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -11;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+
+        /** Test data contains duplicates **/
+        int[] tempArr = new int[5];
+        SelectionSort temp1 = new SelectionSort();
+        tempArr = temp1.basicSelectionSort(arr);
+        for(int i = 0; i < arr.length; i++) {
+        	assertTrue(arr[i] != Sortedarr[i]);
+        	assertFalse(arr[i] == Sortedarr[i]);
+
+        }
+
+
+}
+}
